0e3342c15f963b977b94fb46577a2e054117f1ee
Charlie (Mac) <2712cgc@gmail.com>
2023-09-15 19:35:31 +0100

    New level added

--- source/main.lua
+++ source/main.lua
@@ -1,12 +1,17 @@
 --CoreLibs
-import 'CoreLibs/object'
-import 'CoreLibs/graphics'
-import 'CoreLibs/sprites'
-import 'CoreLibs/timer'
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
 
 -- Libraries
-import 'scripts/libraries/AnimatedSprite'
-import 'scripts/libraries/LDtk'
+import "scripts/libraries/AnimatedSprite"
+import "scripts/libraries/LDtk"
+
+-- Game
+import "scripts/GameScene"
+
+GameScene()
 
 -- PlayDate constants
 local pd <const> = playdate
@@ -14,6 +19,6 @@ local gfx <const> = playdate.graphics
 
 -- Game update loop
 function pd.update()
-    gfx.sprite.update()
-    pd.timer.updateTimers()
+	gfx.sprite.update()
+	pd.timer.updateTimers()
 end
---
+++ source/scripts/GameScene.lua
@@ -0,0 +1,37 @@
+-- PlayDate & LDtk constants
+local gfx <const> = playdate.graphics
+local ldtk <const> = LDtk
+
+-- Load the level
+ldtk.load("levels/level_one.ldtk", false)
+
+-- Create the game scene class
+class('GameScene').extends()
+
+-- Initialise the game scene class
+function GameScene:init()
+	self:goToLevel("Level_0")
+end
+
+-- Go to the level specified
+function GameScene:goToLevel(level_name)
+	gfx.sprite.removeAll()
+
+	for layer_name, layer in pairs(ldtk.get_layers(level_name)) do
+		if layer.tiles then
+			local tilemap = ldtk.create_tilemap(level_name, layer_name)
+			local layerSprite = gfx.sprite.new()
+
+			layerSprite:setTilemap(tilemap)
+			layerSprite:setCenter(0, 0)
+			layerSprite:moveTo(0, 0)
+			layerSprite:setZIndex(layer.zIndex)
+			layerSprite:add()
+
+			local emptyTiles = ldtk.get_empty_tileIDs(level_name, "Solid", layer_name)
+			if emptyTiles then
+				gfx.sprite.addWallSprites(tilemap, emptyTiles)
+			end
+		end
+	end
+end
